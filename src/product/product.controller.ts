d1c7ca7a9850e06c735c00370f2f67cecd044ea5
me.himanshu.royal <me.himanshu.royal@gmail.com>
2022-11-11 13:20:37 +0530

    Product swagger documentaion

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,7 +3,8 @@ import { ProductService } from './product.service';
 import { ProductDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { categories } from 'database_Manager';
-import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags, } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags,ApiBearerAuth,ApiQuery } from '@nestjs/swagger';
+import { categories as categoriesTypes,typeOfQuantity } from 'database_Manager/category.config';
 
 
 @ApiTags('Product Module')
@@ -16,20 +17,215 @@ export class ProductController {
     return this.productService.getHello();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post('addproduct')
-  async addProduct(@Body() body:ProductDto) {
+  @ApiOperation({ summary: 'create product api' })
+  @ApiBearerAuth()
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'coke',
+        },
+        rating: {
+          type: 'number',
+          example: '5',
+        },
+        price: {
+          type: 'number',
+          example: '100',
+        },
+        stock: {
+          type: 'number',
+          example: '2',
+        },
+        logo_link: {
+          type: 'string',
+          example: 'Enter your link',
+        },
+        type_of_quantity: {
+          type: 'string',
+          example: 'Packets',
+          enum: typeOfQuantity,
+        },
+        is_packed_item: {
+          type: 'boolean',
+          example: true,
+        },
+        description: {
+          type: 'string',
+          example: 'product description',
+        },
+        additional_field: {
+          type: 'string',
+          example: 'some',
+        },
+        is_visible: {
+          type: 'boolean',
+          example: true,
+        },
+        is_veg: {
+          type: 'boolean',
+          example: true,
+        },
+        category: {
+          type: 'string',
+          example: 'others',
+          enum: categoriesTypes,
+        },
+        keywords: {
+          type: 'string[]',
+          example: ['a', 'b'],
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'created Product',
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'coke',
+        },
+        rating: {
+          type: 'number',
+          example: '5',
+        },
+        price: {
+          type: 'number',
+          example: '100',
+        },
+        stock: {
+          type: 'number',
+          example: '2',
+        },
+        logo_link: {
+          type: 'string',
+          example: 'Enter your link',
+        },
+        type_of_quantity: {
+          type: 'string',
+          example: 'Packets',
+          enum: typeOfQuantity,
+        },
+        is_packed_item: {
+          type: 'boolean',
+          example: true,
+        },
+        description: {
+          type: 'string',
+          example: 'product description',
+        },
+        additional_field: {
+          type: 'string',
+          example: 'some',
+        },
+        is_visible: {
+          type: 'boolean',
+          example: true,
+        },
+        is_veg: {
+          type: 'boolean',
+          example: true,
+        },
+        category: {
+          type: 'string',
+          example: 'others',
+          enum: categoriesTypes,
+        },
+        keywords: {
+          type: 'string[]',
+          example: ['a', 'b'],
+        },
+      },
+    },
+  })
+  async addProduct(@Body() body: ProductDto) {
     return await this.productService.addProduct(body);
   }
 
-//   @UseGuards(AuthGuard('jwt'))
-   @Get('all')
-   async getAlProduct(@Query() query) {
-     return await this.productService.getAllProduct(query);
-   }
+  @Get('all')
+  @ApiOperation({ summary: 'get all product' })
+  @ApiQuery({ name: 'name', type: 'string', required: false })
+  @ApiQuery({ name: 'category', type: 'string', required: false })
+  @ApiResponse({
+    status: 200,
+    description: 'filter product',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            example: 'coke',
+          },
+          rating: {
+            type: 'number',
+            example: '5',
+          },
+          price: {
+            type: 'number',
+            example: '100',
+          },
+          stock: {
+            type: 'number',
+            example: '2',
+          },
+          logo_link: {
+            type: 'string',
+            example: 'Enter your link',
+          },
+          type_of_quantity: {
+            type: 'string',
+            example: 'Packets',
+            enum: typeOfQuantity,
+          },
+          is_packed_item: {
+            type: 'boolean',
+            example: true,
+          },
+          description: {
+            type: 'string',
+            example: 'product description',
+          },
+          additional_field: {
+            type: 'string',
+            example: 'some',
+          },
+          is_visible: {
+            type: 'boolean',
+            example: true,
+          },
+          is_veg: {
+            type: 'boolean',
+            example: true,
+          },
+          category: {
+            type: 'string',
+            example: 'others',
+            enum: categoriesTypes,
+          },
+          keywords: {
+            type: 'string[]',
+            example: ['a', 'b'],
+          },
+        },
+      },
+    },
+  })
+  async getAlProduct(@Query() query) {
+    return await this.productService.getAllProduct(query);
+  }
 
-   @Get('category')
-   async getProductCategory() {
-     return categories;
-   }
-
+  @Get('category')
+  async getProductCategory() {
+    return categories;
+  }
 }
+                            
